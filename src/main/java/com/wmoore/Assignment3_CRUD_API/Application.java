ce9d61109d8ebd3f007be9ffe21b9616ad656263
William J Moore III <WilliamJMooreIII@proton.me>
2026-02-28 13:04:57 -0500

    Added logger to Application and created wrapper for starting spring in terminal

---
+++ src/main/java/com/wmoore/Assignment3_CRUD_API/Application.java
@@ -0,0 +1,18 @@
+package com.wmoore.Assignment3_CRUD_API;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		log.info("Application Started Successfully!");
+	}
+
+}
