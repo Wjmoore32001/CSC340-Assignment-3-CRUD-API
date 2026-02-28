a09c63c91f2c28de7b97ad1b558f4f5be1c6370a
William J Moore III <WilliamJMooreIII@proton.me>
2026-02-28 08:30:21 -0500

    Spring Initializr

---
+++ src/main/java/com/wmoore/Assignment3_CRUD_API/Assignment3CrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.wmoore.Assignment3_CRUD_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment3CrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment3CrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wmoore/Assignment3_CRUD_API/Assignment3CrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmoore.Assignment3_CRUD_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment3CrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
