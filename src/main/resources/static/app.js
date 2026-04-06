45a36381d438b4bf423673c47002ae6192aed930
William Moore <WilliamJMooreIII@proton.me>
2026-04-06 12:42:31 -0400

    Javascript to populate home gallary

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,36 @@
+
+// Enpoint:
+//  /characters/get-character/all 
+const characterList = document.getElementById("character-list");
+fetch('/characters/get-character/all').then(response => {
+  console.log("got response");
+  return response.json();
+})
+  .then(data => {
+    console.log("got data");
+    console.log(data);
+    characterList.innerHTML = "";
+    data.forEach(character => {
+      const divRow = document.createElement("div");
+      divRow.className = "row mb-5";
+      const divColumn1 = document.createElement("div");
+      divColumn1.className = "col-6 col-lg-6";
+      divColumn1.innerHTML = `
+        <img src="${character.imageUrl}" class="img-fluid" alt="Itadori holding bamboo">
+      `;
+
+      const divColumn2 = document.createElement("div");
+      divColumn2.className = "col-6 col-lg-6";
+      divColumn2.innerHTML = `
+        <p class="fs-4 mb-5">
+          <span class="h3 fw-bold">${character.name}</span>
+          ${character.description}
+        </p>
+      `;
+      divRow.appendChild(divColumn1);
+      divRow.appendChild(divColumn2);
+      characterList.appendChild(divRow);
+      //
+    });
+
+  });
